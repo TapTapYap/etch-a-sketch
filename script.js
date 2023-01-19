d55bef606b679a00011f1b231e02b0823d283ec3
Patrick Oser <patrick.oser148@gmail.com>
2023-01-19 18:35:14 -0500

    Added function to make grid.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const gridContainer = document.getElementById('gridContainer');
+
+function makeGrid(numDivs) {
+    for (let i = 0; i < numDivs; i++) {
+        let block = document.createElement('div');
+        block.classList.add('block');
+        gridContainer.appendChild('block');
+    }
+}
+
+makeGrid(64);
