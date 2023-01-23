8ff2036d98739ed20120bdba0fefc735a742ac47
Patrick Oser <patrick.oser148@gmail.com>
2023-01-23 18:33:47 -0500

    Added another loop within function 'makeRow'.

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 const gridContainer = document.getElementById('gridContainer');
+let column = ;
 
 function makeGrid() {
     makeColumn();
     makeRow();
 }
 
-function makeColumn(numDivs) {
-    for (let i = 0; i < numDivs; i++) {
+function makeColumn(columnDivs) {
+    for (let i = 0; i < columnDivs; i++) {
         let column = document.createElement('div');
         column.classList.add('column');
         gridContainer.appendChild(column);
     }
 }
-function makeRow(numDivs) {
-    for (let j = 0; j < numDivs; j++) {
-        let row = document.createElement('div');
-        row.classList.add('row');
-        gridContainer.appendChild(row);
+function makeRow(rowDivs) {
+    for (let r = 0; r < column.legnth; r++) {
+        for (let j = 0; j < rowDivs; j++) {
+            let row = document.createElement('div');
+            row.classList.add('row');
+            column[j].gridContainer.appendChild(row);
+        }
     }
 }
 
-makeGrid(64);
+makeGrid(16);
