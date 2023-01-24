5addee56e91dd518aaa7ad21a5da065121f10c98
Patrick Oser <patrick.oser148@gmail.com>
2023-01-24 18:15:17 -0500

    "Combined the three functions into one"
    "After combining the three functions I also appended the
    rows to the column divs. This seemed to fix the issue I
    was having with the rows not showing up. Using margins to
    distinguish between the blocks instead of borders also helped."

--- script.js
+++ script.js
@@ -2,26 +2,18 @@ const gridContainer = document.getElementById('gridContainer');
 let column = document.getElementsByClassName("column");
 let row = document.getElementsByClassName("row");
 
-function makeGrid() {
-    makeColumn(16);
-    makeRow(16);
-}
-
-function makeColumn(columnDivs) {
-    for (let i = 0; i < columnDivs; i++) {
+function makeGrid(rowDivs, columnDivs) {
+    for (let r = 0; r < columnDivs; r++) {
         let column = document.createElement('div');
         column.classList.add('column');
-        gridContainer.appendChild(column);
-    }
-}
-function makeRow(rowDivs) {
-    for (let r = 0; r < column.legnth; r++) {
         for (let j = 0; j < rowDivs; j++) {
             let row = document.createElement('div');
             row.classList.add('row');
-            column[j].gridContainer.appendChild(row);
+            column.appendChild(row);
         }
+        gridContainer.appendChild(column);
     }
+    document.body.appendChild(gridContainer);
 }
 
-makeGrid();
+makeGrid(64, 64);
