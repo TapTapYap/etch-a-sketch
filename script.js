8ef38ebddbee8dfff17ea4c9297492784878d184
Patrick Oser <patrick.oser148@gmail.com>
2023-01-25 18:38:03 -0500

    Added buttons for changing colors of grid blocks.

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const gridContainer = document.getElementById('gridContainer');
 let column = document.getElementsByClassName("column");
 let row = document.getElementsByClassName("row");
 
+let blackBtn = document.querySelector(".blackBtn");
+let rainbowBtn = document.querySelector(".rainbowBtn");
+
 function makeGrid(rowDivs, columnDivs) {
     for (let r = 0; r < columnDivs; r++) {
         let column = document.createElement('div');
