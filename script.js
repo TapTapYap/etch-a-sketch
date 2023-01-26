f28927b6e011bc6adb1957bd2bb670e65ac6b68b
Patrick Oser <patrick.oser148@gmail.com>
2023-01-26 13:35:31 -0500

    Added prompt to ask user what size grid they want.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let row = document.getElementsByClassName("row");
 let blackBtn = document.querySelector(".blackBtn");
 let rainbowBtn = document.querySelector(".rainbowBtn");
 
-/* Mouse events */
+/* Mouse events to determine if the grids blocks should change color */
 
 let mouseDown = false;
 document.body.onmousedown = function() {
@@ -17,19 +17,28 @@ document.body.onmouseup = function() {
     mouseDown = false;
 }
 
+/* Prompt user to input what size they want the grid to be. */
+
+
+
+/* Function to construct the grid. */
+
 function makeGrid(rowDivs, columnDivs) {
     for (let r = 0; r < columnDivs; r++) {
         let column = document.createElement('div');
         column.classList.add('column');
         for (let j = 0; j < rowDivs; j++) {
             let row = document.createElement('div');
-            row.addEventListener('mousedown', changeColor)
+            row.addEventListener('mousedown', changeColor);
+            row.addEventListener('mouseover', changeColor);
             row.classList.add('row');
             column.appendChild(row);
         }
         gridContainer.appendChild(column);
     }
 }
+
+/* Function used to determine which color to display */
 
 function changeColor(e) {
     if (mouseDown) {
@@ -40,4 +49,13 @@ function changeColor(e) {
         e.target.style.backgroundColor = `rgb(${color1},${color2},${color3})`;
     }
 }
-makeGrid(64, 64);
+
+function getSize(e) {
+    if (e > 100) {
+        alert("Size can not be greater than 100")
+    }
+}
+
+/* Call upon the makeGrid function */
+
+makeGrid(50, 50);
