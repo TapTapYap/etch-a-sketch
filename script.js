085055c087ce10c2dbbb97f9f35dee7e827212d3
Patrick Oser <patrick.oser148@gmail.com>
2023-01-26 12:46:38 -0500

    Added mouseDown and changeColor functions.

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ const gridContainer = document.getElementById('gridContainer');
 let column = document.getElementsByClassName("column");
 let row = document.getElementsByClassName("row");
 
+/* Buttons for changing color and resetting the grid */
+
 let blackBtn = document.querySelector(".blackBtn");
 let rainbowBtn = document.querySelector(".rainbowBtn");
+
+/* Mouse events */
+
+let mouseDown = false;
+document.body.onmousedown = function() {
+    mouseDown = true;
+}
+document.body.onmouseup = function() {
+    mouseDown = false;
+}
 
 function makeGrid(rowDivs, columnDivs) {
     for (let r = 0; r < columnDivs; r++) {
@@ -11,12 +23,21 @@ function makeGrid(rowDivs, columnDivs) {
         column.classList.add('column');
         for (let j = 0; j < rowDivs; j++) {
             let row = document.createElement('div');
+            row.addEventListener('mousedown', changeColor)
             row.classList.add('row');
             column.appendChild(row);
         }
         gridContainer.appendChild(column);
     }
-    document.body.appendChild(gridContainer);
 }
 
+function changeColor(e) {
+    if (mouseDown) {
+        let color1 = Math.floor(Math.random() * 256);
+        let color2 = Math.floor(Math.random() * 256);
+        let color3 = Math.floor(Math.random() * 256);
+
+        e.target.style.backgroundColor = `rgb(${color1},${color2},${color3})`;
+    }
+}
 makeGrid(64, 64);
